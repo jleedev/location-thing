8555dce031c68755edddd7f9edcd9d8a413b9748
Josh Lee <jleedev@gmail.com>
2010-12-21 23:43:41 -0500

    Automatically pauses locations with the activity.
    But automatically resumes without being asked to.

--- src/com/example/myproject/MyActivity.java
+++ src/com/example/myproject/MyActivity.java
@@ -12,66 +12,95 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
 
-public class MyActivity extends Activity {
-	
+public class MyActivity extends Activity implements LocationListener {
+
+	ListView listView;
+	ArrayAdapter<String> listAdapter;
+	LocationManager locationManager;
+	Button button_start;
+	Button button_stop;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		final ListView listView = (ListView) findViewById(R.id.listview);
-		final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.test_list_item, new ArrayList<String>());
+		listView = (ListView) findViewById(R.id.listview);
+		listAdapter = new ArrayAdapter<String>(this, android.R.layout.test_list_item, new ArrayList<String>());
 		listView.setAdapter(listAdapter);
 
-		final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		final LocationListener locationListener = new LocationListener() {
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-			public void onLocationChanged(Location location) {
-				listAdapter.add("Location " + location + "\n");
-				listView.smoothScrollToPosition(listAdapter.getCount());
-			}
-
-			public void onProviderDisabled(String provider) {
-				listAdapter.add("Disabled " + provider + "\n");
-				listView.smoothScrollToPosition(listAdapter.getCount());
-			}
-
-			public void onProviderEnabled(String provider) {
-				listAdapter.add("Enabled " + provider + "\n");
-				listView.smoothScrollToPosition(listAdapter.getCount());
-			}
-
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-				listAdapter.add("Changed " + provider + " " + status + "\n");
-				listView.smoothScrollToPosition(listAdapter.getCount());
-			}
-
-		};
-		
-		final Button button_start = (Button) findViewById(R.id.button_start);
-		final Button button_stop = (Button) findViewById(R.id.button_stop);
+		button_start = (Button) findViewById(R.id.button_start);
+		button_stop = (Button) findViewById(R.id.button_stop);
 		
 		button_stop.setEnabled(false);
 		
 		button_start.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				locationManager.requestLocationUpdates(
-						LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-				locationManager.requestLocationUpdates(
-						LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-				button_start.setEnabled(false);
-				button_stop.setEnabled(true);
+				resumeUpdates();
 			}
 		});
 		
 		button_stop.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				locationManager.removeUpdates(locationListener);
-				button_start.setEnabled(true);
-				button_stop.setEnabled(false);
+				pauseUpdates();
 			}
 		});
 	}
+
+	void append(String text) {
+		listAdapter.add(text);
+		listView.smoothScrollToPosition(listAdapter.getCount());
+	}
+
+	public void pauseUpdates() {
+		locationManager.removeUpdates(this);
+		button_start.setEnabled(true);
+		button_stop.setEnabled(false);
+	}
+
+	public void resumeUpdates() {
+		locationManager.requestLocationUpdates(
+			LocationManager.GPS_PROVIDER, 0, 0, this);
+		locationManager.requestLocationUpdates(
+			LocationManager.NETWORK_PROVIDER, 0, 0, this);
+		button_start.setEnabled(false);
+		button_stop.setEnabled(true);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		pauseUpdates();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		resumeUpdates();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		append("Location " + location);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		append("Disabled " + provider);
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		append("Enabled " + provider);
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status,
+			Bundle extras) {
+		append("Changed " + provider + " " + status);
+	}
+
 }
